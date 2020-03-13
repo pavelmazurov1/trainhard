6c445616df6549001ad2d338b39fdd84dd25f8ce
mpv <mpv@barl.ru>
2020-03-13 13:57:41 +0300

    Тестовый скрипт для фпс

--- unity/Assets/scripts/TestFirstPersionUnit.cs
+++ unity/Assets/scripts/TestFirstPersionUnit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TestFirstPersionUnit : MonoBehaviour
 {
     CharacterController characterController;
+    public Camera characterCamera;
 
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
@@ -22,14 +23,20 @@ public class TestFirstPersionUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * lookSensivity, Input.GetAxis("Mouse Y") * Time.deltaTime * lookSensivity);
+        //
+        transform.Rotate( 0, Input.GetAxis("Mouse X") * Time.deltaTime * lookSensivity, 0);
+        characterCamera.transform.Rotate(-Input.GetAxis("Mouse Y") * Time.deltaTime * lookSensivity, 0, 0);
 
         if (characterController.isGrounded)
         {
             // We are grounded, so recalculate
             // move direction directly from axes
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            
+            moveDirection = transform.forward;
+            //moveDirection.x *= Input.GetAxis("Horizontal");
+            moveDirection *= Input.GetAxis("Vertical");
+            moveDirection += Input.GetAxis("Horizontal") * transform.right;
             moveDirection *= speed;
 
             if (Input.GetButton("Jump"))
