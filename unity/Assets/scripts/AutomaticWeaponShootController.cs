638bd4a5420700413875bd552574c6b87c028d04
mpv <mpv@barl.ru>
2020-03-24 16:38:21 +0300

    добавлен звук выстрела для ак47

--- unity/Assets/scripts/AutomaticWeaponShootController.cs
+++ unity/Assets/scripts/AutomaticWeaponShootController.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class AutomaticWeaponShootController : MonoBehaviour
 {
     public ParticleSystem blustPrticles;
+    public AudioClip shotSound;
 
+    public float fireRatePerSecond;
     public bool isShooting = false;
+    private AudioSource audioSource;
+    private ParticleSystem.EmissionModule emissionModule;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        emissionModule = blustPrticles.emission;
+        StartCoroutine(shotRoutine());
+    }
+
+    IEnumerator shotRoutine()
+    {
+        while (true)
+        {
+            if (isShooting)
+            {
+                audioSource.PlayOneShot(shotSound);
+                yield return new WaitForSeconds(1f / fireRatePerSecond);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        emissionModule.rate = fireRatePerSecond;
+        
         var firePressed = Input.GetButton("Fire1");
         if (isShooting && !firePressed)
         {
